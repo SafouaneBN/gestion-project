573e4d0768e46b01dd6d9e734eef556fdf556954
safoine bnou <safoineps6@gmail.com>
2022-07-14 15:22:58 +0100

    v2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.master')
-
+@section("title","dash")
 @section('content')
-<h1>hello</h1>
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+						<!--begin::Container-->
+						<div id="kt_content_container" class="container-xxl">
+							<!--begin::Row-->
+							
+							<!--end::Row-->
+							<!--begin::Row-->
+							<div class="row gy-5 g-xl-8">
+															
+							<h1>test</h1>
+							</div>
+							
+						</div>
+						<!--end::Container-->
+					</div>
 @endsection
